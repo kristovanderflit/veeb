e1549d4bde085888c61dd1c909fea157ec1a68f3
kristo.vanderflit <kristo.vanderflit@khk.ee>
2018-01-30 10:29:35 +0200

    kommentaari näidised

---
+++ praks1/väljund.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kristo.vanderflit
+ * Date: 30.01.2018
+ * Time: 10:24
+ */
+// ühe realine kommentaar
+
+/*
+ * mitme
+ * realine
+ * kommentaar
+ */
+
