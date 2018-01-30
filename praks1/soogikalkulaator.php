2e48cc3108c90bce080f4b63e881b3fd20e40980
kristo.vanderflit <kristo.vanderflit@khk.ee>
2018-01-30 11:39:51 +0200

    töötav kalkulaator

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -9,3 +9,10 @@ $soogiHind = 2.55; // hind, EUR
 $soodustus = 15; // %
 $toetus = 1.80; // EUR
 
+if($soogiHind > 0 and $soodustus > 0 and $toetus > 0){
+    $soodusHind = $soogiHind * ((100 - $soodustus ) / 100);
+    $hindOpilasele = $soodusHind - $toetus;
+    echo 'Soogi hind opilasele = '.$hindOpilasele.'<br />';
+} else {
+    echo 'Andmed ei ole taielikult<br />';
+}
