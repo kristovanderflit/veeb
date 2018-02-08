e479f45b014a8619d55185058629ffcd2133d6e7
kristo.vanderflit <kristo.vanderflit@khk.ee>
2018-02-08 11:38:46 +0200

    masiivi kuju kasutatu v''rtused

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -29,30 +29,18 @@ function soogiHind($taisHind, $soodusKaart = false, $kasOledOpilane = false){
 
 // testimiseks paneme erinevad vaartused paika
 // kasutame selleks massiivid (soodusKaart, kasOledOpilane)
-$opilane = array(true, true); // olemas soodus  - $opilane[0] ja oled opilane - $opilane[1]
-$opetaja = array(true, false); // olemas soodus aga opilane ei ole
-$kulaline = array(false, false); // pole soodustust ja pole opilane
-
-// kutsume finktsiooni toole
-// kui oled opilane
-$soogiHind = soogiHind(2.65, $opilane[0], $opilane[1]);
-echo 'Prae hind opilasele = '.round($soogiHind, 2).'<br />';
-//Kui olemas kliendi kaart, aga pole opilane
-$soogiHind = soogiHind(2.65,$opetaja[0], $opetaja[1]);
-echo 'Prae hind sooduskaardi omanikule = '.round($soogiHind, 2).'<br />';
-// Kui ei ole kliendi kaart
-$soogiHind = soogiHind(2.65, $kulaline[0], $kulaline[1]);
-echo 'Prae hind  = '.round($soogiHind, 2).'<br />';
-
-
-/*
-$soogiHind = 2.55; // hind, EUR
-
-if($soogiHind > 0 and $soodustusProtsent > 0 and $toetus > 0){
-    $soodusHind = $soogiHind * ((100 - $soodustusProtsent ) / 100);
-    $hindOpilasele = $soodusHind - $toetus;
-    echo 'Soogi hind opilasele = '.$hindOpilasele.'<br />';
-} else {
-    echo 'Andmed ei ole taielikult<br />';
+// kasutajad on tabel (massiiv), kus
+//1. real on opilase andmed
+//2. real on opetaja andmed
+//3. real on kulaline andmed
+$kasutajad = array(
+    array(true, true),
+    array(true, false),
+    array(false, false)
+);
+// vaatame $kasutaja masiivi l'bi
+// for (tjm defineerimine; tjm kehtivuse kontroll; tjm suurendamine/v'hendamine)
+for($i = 0; $i < count($kasutajad); $i++){
+    $soogiHind = soogiHind(2.65, $kasutajad[$i][0], $kasutajad[$i][1]);
+    echo 'Prae hind  = '.round($soogiHind, 2).'<br />';
 }
-*/
