4e90e5e340a6c2d6ca25d7f89f2a34df8b7e1938
kristo.vanderflit <kristo.vanderflit@khk.ee>
2018-02-21 09:12:10 +0200

    m'ng

--- praks1/mang.php
+++ praks1/mang.php
@@ -6,17 +6,31 @@
  * Time: 12:37
  */
 
+// määrame katsete arvu muutujat
+$katseteArv = $_POST['katseteArv'];
+//serveri arv
+$serveriArv = $_POST['serveriArv'];
+
+// kontrollime antud väärtuse olemasolu
+$katseteArv = isset($katseteArv) ? ++$katseteArv : 0;
+$serveriArv = isset($serveriArv) ? ++$serveriArv : rand(1, 50);
+// trükime andmetega täidetud vorm
+
+
+
 echo '
     <form method="post" action="'.$_SERVER['PHP_SELF'].'">
         Sisesta arv vahemikus 1-50: 
         <input type="number" name="kasutajaArv"><br />
+        <input type="hidden" name="katseteArv" value="'.$katseteArv.'">
+        <input type="hidden" name="serveriArv" value="'.$serveriArv.'">
         <input type="submit" value="Kontrolli">
     </form>
 ';
 // kontrollime, kas arv on edastatud
 if(!empty($_POST['kasutajaArv'])){
     // määrame arvu
-    $serveriArv = 10;
+
     $kasutajaArv = $_POST['kasutajaArv'];
     if($kasutajaArv > $serveriArv){
         echo 'Pakutud väärtus on suurem<br />';
@@ -28,6 +42,7 @@ if(!empty($_POST['kasutajaArv'])){
         if($kasutajaArv == $serveriArv){
             echo 'Arvasid ära!<br />';
             echo 'Arv on '.$serveriArv.'<br />';
+            echo 'Arvu ära aramiseks läks '.$katseteArv.' korda<br />';
             exit;
         }
         echo 'Oled juba hästi lähedal<br />';
